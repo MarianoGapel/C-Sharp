ffbf98ba82cbd5bd58475b78d40a399b59d383ba
MarianoGapel <marianogapel@gmail.com>
2024-08-01 19:42:04 -0300

    Actualizado

--- 4 CONDICIONALES II/EJ5/Program.cs
+++ 4 CONDICIONALES II/EJ5/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Hacer un programa que solicite el ingreso de las notas del primer parcial y del segundo parcial de una alumna de programación. 
+// El programa deberá analizar las notas y emitir la situación de la alumna según la siguiente escala:
+// a. Si tiene 8 o más en ambos parciales, emitir “aprobación directa”.
+// b. Si no tiene 8 o más en ambos pero tiene aprobados ambos parciales (se aprueba con 6 o más), emitir “rinde examen final”.
+// c. Si tiene menos de 6 en alguno de los dos parciales, emitir “debe recuperar”.
+// El programa debe emitir solo un cartel, el que corresponda.
+
+using System;
 
 namespace EJ5
 {
@@ -6,7 +13,18 @@ namespace EJ5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nota1, nota2;
+            Console.WriteLine("Ingrese las notas del primer y del segundo parcial");
+            nota1 = int.Parse(Console.ReadLine());
+            nota2 = int.Parse(Console.ReadLine());
+
+            if (nota1 >= 8 && nota2 >= 8)
+                Console.WriteLine("Aprobacion directa");
+            else if (nota1 >= 6 && nota2 >= 6)
+                Console.WriteLine("Rinde examen final");
+            else
+                Console.WriteLine("Debe recuperar");             
         }
+
     }
 }
