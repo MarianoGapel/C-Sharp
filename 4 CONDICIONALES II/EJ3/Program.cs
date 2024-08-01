6ec339e9cbd1a0044be0e09c9de27a8e93878cb1
MarianoGapel <marianogapel@gmail.com>
2024-08-01 19:23:56 -0300

    Actualizado

--- 4 CONDICIONALES II/EJ3/Program.cs
+++ 4 CONDICIONALES II/EJ3/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Hacer un programa para ingresar dos números. Si el segundo es distinto de cero, calcular la división del primero por el segundo y mostrar el resultado por pantalla; 
+// caso contrario, emitir un cartel aclarando que no se puede dividir por cero.
+
+using System;
 
 namespace EJ3
 {
@@ -6,7 +9,17 @@ namespace EJ3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float nro1, nro2, r;
+            Console.WriteLine("Ingrese dos numeros");
+            nro1 = float.Parse(Console.ReadLine());
+            nro2 = float.Parse(Console.ReadLine());
+
+            r = 0;
+            if (nro2 != 0) {  
+                r = nro1 / nro2;
+                Console.WriteLine("El resultado es: " + r);
+            } else  
+                Console.WriteLine("No se puede dividir por cero.");   
         }
     }
 }
--- 4 CONDICIONALES II/EJ4/Program.cs
+++ 4 CONDICIONALES II/EJ4/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//Un importante negocio de desinfectante líquido realiza descuentos dependiendo de la cantidad de litros vendidos según la siguiente escala:
+//a. Si vende menos de 100 litros, no hay descuento.
+//b. Si vende entre 101 y 300 litros, el descuento es del 10%.
+//c. Si vende entre 301 y 500 litros, el descuento es del 15%.
+//d. Finalmente, si la venta es de más de 500 litros, el descuento es del 25%.
+//Hacer un programa que solicite el ingreso del importe total de la venta y la cantidad de litros vendidos y calcule y emita el importe con el descuento aplicado.
+
+using System;
 
 namespace EJ4
 {
@@ -6,7 +13,28 @@ namespace EJ4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float l, montoInicial, descuento, montoFinal;
+            Console.WriteLine("Ingrese la cantidad de litros vendidos");
+            l = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el importe de la venta:");
+            montoInicial = float.Parse(Console.ReadLine());
+
+            descuento = 0;
+            montoFinal = 0;
+
+            if (l > 500) { 
+                descuento = montoInicial * 0.25F;
+                montoFinal = montoInicial - descuento;
+            }if (l >= 301 && l <= 500) { 
+                descuento = montoInicial * 0.15F;
+                montoFinal = montoInicial - descuento;
+            }if (l >= 101 && l <= 300) { 
+                descuento = montoInicial * 0.10F;
+                montoFinal = montoInicial - descuento;
+            }if (l < 100)
+                montoFinal = montoInicial;
+
+            Console.WriteLine("El importe final con el descuento aplicado es de: " + montoFinal);
         }
     }
 }
