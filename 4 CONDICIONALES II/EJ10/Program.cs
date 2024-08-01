3a40f254452b8b9b9a5d5b6700b06eade474e16b
MarianoGapel <marianogapel@gmail.com>
2024-08-01 20:05:33 -0300

    Actualizado

--- 4 CONDICIONALES II/EJ10/Program.cs
+++ 4 CONDICIONALES II/EJ10/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Hacer un programa que solicite cuatro números y emitir un cartel aclaratorio si son todos iguales entre sí, caso contrario, no emitir nada.
+
+using System;
 
 namespace EJ10
 {
@@ -6,7 +8,16 @@ namespace EJ10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nro1, nro2, nro3, nro4;
+            Console.WriteLine("Ingrese 4 numeros");
+            nro1 = int.Parse(Console.ReadLine());
+            nro2 = int.Parse(Console.ReadLine());
+            nro3 = int.Parse(Console.ReadLine());
+            nro4 = int.Parse(Console.ReadLine());
+
+            if (nro1 == nro2 && nro2 == nro3 && nro3 == nro4)
+                Console.WriteLine("Son iguales.");
+
         }
     }
 }
--- 4 CONDICIONALES II/EJ11/Program.cs
+++ 4 CONDICIONALES II/EJ11/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Hacer un programa para ingresar tres números y luego mostrarlos ordenados de menor a mayor.
+
+using System;
 
 namespace EJ11
 {
@@ -6,7 +8,34 @@ namespace EJ11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nro1, nro2, nro3, menor, medio, mayor;
+            Console.WriteLine("ingresar 3 numeros");
+            nro1 = int.Parse(Console.ReadLine());
+            nro2 = int.Parse(Console.ReadLine());
+            nro3 = int.Parse(Console.ReadLine());
+            
+            mayor = 0;
+            menor = 0;
+            medio = 0;
+
+            if (nro1 > nro2){
+                mayor = nro1;
+                menor = nro2;
+            }else {
+                mayor = nro2;
+                menor = nro1;
+            }if (nro3 > mayor)
+                mayor = nro3;
+            if (nro3 < menor)
+                menor = nro3;
+            else 
+                medio = nro3;
+            if (nro1 > menor && nro1 < mayor)
+                medio = nro1;
+            if (nro2 > menor && nro2 < mayor)
+                medio = nro2;
+            
+            Console.WriteLine("Los numeros ordenados de menor a mayor son: " + menor + ", " + medio + ", " + mayor);
         }
     }
 }
