e05f65813f442f014d6988430e4ab6958595314a
MarianoGapel <marianogapel@gmail.com>
2024-08-01 22:52:18 -0300

    Actualizado

--- 5 CICLOS/1 FOR/EJ6/Program.cs
+++ 5 CICLOS/1 FOR/EJ6/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Hacer un programa que solicite UN número y luego calcule y emita un cartel aclaratorio si el mismo es primo o no es primo.
+// Nota: un número es primo cuando es divisible únicamente por 1 y por sí mismo.
+
+
+using System;
 
 namespace EJ6
 {
@@ -6,7 +10,22 @@ namespace EJ6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n, con;
+            con = 0;
+            Console.WriteLine("Ingrese un numero:");
+            n = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= n; x++)
+            {
+                if (n % x == 0)
+                    con++;
+            }
+            if (con == 2)
+                Console.WriteLine("Es primo.");
+            else
+                Console.WriteLine("No es primo.");
         }
     }
 }
+// El código verifica si el número ingresado es primo o no. El bucle for se inicia desde x = 1 y va hasta x = n, contando cuántos divisores tiene el número n.
+// Si el número n tiene exactamente dos divisores (1 y el propio número), entonces se considera primo, ya que los números primos solo tienen dos divisores. 
+// De lo contrario, si tiene más de dos divisores, se considera compuesto y, por lo tanto, no es primo.
--- 5 CICLOS/1 FOR/EJ7/Program.cs
+++ 5 CICLOS/1 FOR/EJ7/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Hacer un programa que solicite 10 números y luego mostrar por pantalla el máximo de ellos y la posición en la que fue ingresado.
+
+using System;
 
 namespace EJ7
 {
@@ -6,7 +8,22 @@ namespace EJ7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int pos, mayor, n;
+            mayor = 0;
+            pos = 0;
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine("ingrese un numero:");
+                n = int.Parse(Console.ReadLine());
+                if (x == 0) { // Utilizo corchetes dentro del if para que se tenga en cuenta las dos lineas siguientes.
+                    mayor = n;
+                    pos = 1;    
+                }else if (n > mayor){
+                    mayor = n;
+                    pos = x+1;
+                }    
+            }
+            Console.WriteLine("El mayor es: " + mayor + ". Ubicado en la posicion: " + pos);
         }
-    }
+    } 
 }
